38ae519df86e47f2c77f643b5c003cf8f7454513
KingGhidorah07 <kingghidorahdev@gmail.com>
2024-10-26 14:51:55 -0600

    Initial Commit

---
+++ ValidateImage/ValidateImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValidateImageApp.swift
+//  ValidateImage
+//
+//  Created by KingGhidorah01 on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ValidateImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ValidateImageTests/ValidateImageTests.swift
@@ -0,0 +1,17 @@
+//
+//  ValidateImageTests.swift
+//  ValidateImageTests
+//
+//  Created by KingGhidorah01 on 26/10/24.
+//
+
+import Testing
+@testable import ValidateImage
+
+struct ValidateImageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
